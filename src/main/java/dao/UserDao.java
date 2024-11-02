69d7e9d8a63e4dd3bef38c3589b82f7cfaf11da6
will <demoonly613@gmail.com>
2024-11-02 09:26:48 +0800

    第一版本，userDAO 和他的實作

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -3,6 +3,27 @@ package dao;
 import vo.User;
 
 public interface UserDao {
-	int insert (User user);
+//	int insert (User user);
+
+    // Add user into the database
+    int addUser(User user) throws Exception;
+
+    // Search user by user ID
+    User searchUserById(Integer userId) throws Exception;
+
+    // Search user by email
+    User searchUserByEmail(String email) throws Exception;
+
+    // Delete user by user ID
+    int deleteUserById(Integer userId) throws Exception;
+
+    // Delete user by email
+    int deleteUserByEmail(String email) throws Exception;
+
+    // Update user by user ID
+    int updateUserById(User user) throws Exception;
+
+    // Update user by email
+    int updateUserByEmail(User user) throws Exception;
 	
 }
